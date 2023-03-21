c80069608342563c9edca6901dc6528d4d57986b
Lenny <46480795+0xlny@users.noreply.github.com>
2023-03-21 16:03:33 +0200

    :wrench: Update goerli dev exchange contract (#42)
    * :wrench: Update goerli dev exchange contract
    
    * :white_check_mark: Fix tests

--- src/constants.ts
+++ src/constants.ts
@@ -3,6 +3,6 @@ import { constants } from 'starknet';
 export const STAGING_BASE_URL = 'https://goerli.api.avnu.fi';
 export const BASE_URL = 'https://goerli.api.avnu.fi';
 export const AVNU_ADDRESS: { [chainId: string]: string } = {
-  [constants.StarknetChainId.TESTNET]: '0x5c614428c49b94ab60c90ea55d366d328921c829bbd3ae81d748723750c0931',
+  [constants.StarknetChainId.TESTNET]: '0x06d8cd321dcbbf54512eab67c8a6849faf920077a3996f40bb4761adc4f021d2',
   [`${constants.StarknetChainId.TESTNET}-dev`]: '0x7852cb149218526b12a1ca5a2c443976571c8d9881b343cbf617575a34eec08',
 };
