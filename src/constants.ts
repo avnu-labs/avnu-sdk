1c7975f47877357176c25429c42ecd87fc689cb0
florian-bellotti <florian.bellotti@viacesi.fr>
2023-04-30 18:14:50 +0200

    :sparkles: Adding mainnet address

--- src/constants.ts
+++ src/constants.ts
@@ -3,6 +3,7 @@ import { constants } from 'starknet';
 export const STAGING_BASE_URL = 'https://goerli.api.avnu.fi';
 export const BASE_URL = 'https://goerli.api.avnu.fi';
 export const AVNU_ADDRESS: { [chainId: string]: string } = {
+  [constants.StarknetChainId.MAINNET]: '0x4270219d365d6b017231b52e92b3fb5d7c8378b05e9abc97724537a80e93b0f',
   [constants.StarknetChainId.TESTNET]: '0x6d8cd321dcbbf54512eab67c8a6849faf920077a3996f40bb4761adc4f021d2',
   [`${constants.StarknetChainId.TESTNET}-dev`]: '0x871a5d57d3d89a451edfccb4a925bfbb712f960c69396d19ca12b3916fb1cd',
 };
