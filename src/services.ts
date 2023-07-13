04bd5e603db9ea551b54f48d5074a23fbc52069c
0xlenny <46480795+0xlny@users.noreply.github.com>
2023-07-13 18:47:58 +0400

    :arrow_up: Update starknetjs (#120)
    * :arrow_up: Update starknetjs
    
    * :white_check_mark: Fix build
    
    * :bug: Fix signature verification
    
    * :bug: Fix starknet version
    
    * :fire: Remove not necessary check

--- src/services.ts
+++ src/services.ts
@@ -1,16 +1,14 @@
 import { toBeHex } from 'ethers';
 import qs from 'qs';
-import { AccountInterface, Call, ec, hash, Signature, typedData, uint256 } from 'starknet';
+import { AccountInterface, Call, ec, hash, Signature, stark, typedData, uint256 } from 'starknet';
 import { AVNU_ADDRESS, BASE_URL, STAGING_BASE_URL } from './constants';
 import {
   AvnuOptions,
   BuildSwapTransaction,
   ExecuteSwapOptions,
-  GetPairsRequest,
   GetTokensRequest,
   InvokeSwapResponse,
   Page,
-  Pair,
   Price,
   PriceRequest,
   Quote,
@@ -34,13 +32,15 @@ const parseResponse = <T>(response: Response, avnuPublicKey?: string): Promise<T
   if (avnuPublicKey) {
     const signature = response.headers.get('signature');
     if (!signature) throw new Error('No server signature');
-    const keyPair = ec.getKeyPairFromPublicKey(avnuPublicKey);
     return response
       .clone()
       .text()
       .then((textResponse) => {
         const hashResponse = hash.computeHashOnElements([hash.starknetKeccak(textResponse)]);
-        if (!ec.verify(keyPair, hashResponse, signature.split(','))) throw new Error('Invalid server signature');
+        const formattedSig = signature.split(',').map((s) => BigInt(s));
+        const signatureType = new ec.starkCurve.Signature(formattedSig[0], formattedSig[1]);
+        if (!ec.starkCurve.verify(signatureType, hashResponse, avnuPublicKey))
+          throw new Error('Invalid server signature');
       })
       .then(() => response.json());
   }
@@ -121,8 +121,15 @@ const fetchExecuteSwapTransaction = (
   takerAddress?: string,
   slippage?: number,
   options?: AvnuOptions,
-): Promise<InvokeSwapResponse> =>
-  fetch(`${options?.baseUrl ?? getBaseUrl()}/swap/v1/execute`, {
+): Promise<InvokeSwapResponse> => {
+  let signature: string[] = [];
+
+  if (Array.isArray(takerSignature)) {
+    signature = takerSignature.map((sig) => toBeHex(BigInt(sig)));
+  } else if (takerSignature.r && takerSignature.s) {
+    signature = [toBeHex(BigInt(takerSignature.r)), toBeHex(BigInt(takerSignature.s))];
+  }
+  return fetch(`${options?.baseUrl ?? getBaseUrl()}/swap/v1/execute`, {
     method: 'POST',
     headers: {
       Accept: 'application/json',
@@ -134,9 +141,10 @@ const fetchExecuteSwapTransaction = (
       takerAddress,
       nonce,
       slippage,
-      takerSignature: takerSignature.map((signature) => toBeHex(BigInt(signature))),
+      takerSignature: signature,
     }),
   }).then((response) => parseResponse<InvokeSwapResponse>(response, options?.avnuPublicKey));
+};
 
 /**
  * Build data for executing the exchange through AVNU router
@@ -331,7 +339,8 @@ const executeSwap = async (
   { executeApprove = true, gasless = false, takerSignature, slippage }: ExecuteSwapOptions = {},
   options?: AvnuOptions,
 ): Promise<InvokeSwapResponse> => {
-  if (account.chainId !== quote.chainId) {
+  const chainId = await account.getChainId();
+  if (chainId !== quote.chainId) {
     throw Error(`Invalid chainId`);
   }
 
@@ -342,7 +351,7 @@ const executeSwap = async (
   // /!\ Do not implement this yourself. It will change /!\
   let nonce = undefined;
   if (quote.liquiditySource === 'MARKET_MAKER' || gasless) {
-    const getNonce = buildGetNonce(account.address, account.chainId, options?.dev);
+    const getNonce = buildGetNonce(account.address, chainId, options?.dev);
     const response = await account.callContract(getNonce);
     nonce = response.result[0];
   }
