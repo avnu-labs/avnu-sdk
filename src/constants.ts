e457340b6806eea7c1036105c5d13edceb792020
florian-bellotti <florian.bellotti@viacesi.fr>
2023-09-06 13:44:44 +0200

    :wrench: Change goerli dev contract

--- src/constants.ts
+++ src/constants.ts
@@ -6,5 +6,5 @@ export const AVNU_ADDRESS: { [chainId: string]: string } = {
   [constants.StarknetChainId.SN_MAIN]: '0x4270219d365d6b017231b52e92b3fb5d7c8378b05e9abc97724537a80e93b0f',
   [`${constants.StarknetChainId.SN_MAIN}-dev`]: '0x4270219d365d6b017231b52e92b3fb5d7c8378b05e9abc97724537a80e93b0f',
   [constants.StarknetChainId.SN_GOERLI]: '0x7e36202ace0ab52bf438bd8a8b64b3731c48d09f0d8879f5b006384c2f35032',
-  [`${constants.StarknetChainId.SN_GOERLI}-dev`]: '0x7e36202ace0ab52bf438bd8a8b64b3731c48d09f0d8879f5b006384c2f35032',
+  [`${constants.StarknetChainId.SN_GOERLI}-dev`]: '0x06d8cd321dcbbf54512eab67c8a6849faf920077a3996f40bb4761adc4f021d2',
 };
