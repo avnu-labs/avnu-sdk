beac3627844d2a7f27e6fde463a7737d264f5788
florian-bellotti <florian.bellotti@viacesi.fr>
2023-09-28 20:42:11 +0200

    :bug: Fix BASE_URL

--- src/constants.ts
+++ src/constants.ts
@@ -1,7 +1,7 @@
 import { constants } from 'starknet';
 
 export const STAGING_BASE_URL = 'https://goerli.api.avnu.fi';
-export const BASE_URL = 'https://goerli.api.avnu.fi';
+export const BASE_URL = 'https://starknet.api.avnu.fi';
 export const AVNU_ADDRESS: { [chainId: string]: string } = {
   [constants.StarknetChainId.SN_MAIN]: '0x4270219d365d6b017231b52e92b3fb5d7c8378b05e9abc97724537a80e93b0f',
   [`${constants.StarknetChainId.SN_MAIN}-dev`]: '0x33c21d4cd5db846109448b3e827d175e50b6d449387f76ca7f91a881edca44b',
