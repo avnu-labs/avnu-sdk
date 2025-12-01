ad23d294182f9f5cda6e593d90ccab6810ab57a9
0xlny <lenny@419labs.io>
2025-12-01 18:28:54 +0100

    :wrench: Update versions

--- src/constants.ts
+++ src/constants.ts
@@ -3,9 +3,9 @@ export const BASE_URL = 'https://starknet.api.avnu.fi';
 export const IMPULSE_BASE_URL = 'https://starknet.impulse.avnu.fi';
 export const SEPOLIA_IMPULSE_BASE_URL = 'https://sepolia.impulse.avnu.fi';
 
-export const SWAP_API_VERSION = 'v3';
 export const TOKEN_API_VERSION = 'v1';
 export const IMPULSE_API_VERSION = 'v1';
+export const SWAP_API_VERSION = 'v3';
 export const PRICES_API_VERSION = 'v3';
-export const STAKING_API_VERSION = 'v2';
-export const DCA_API_VERSION = 'v1';
+export const STAKING_API_VERSION = 'v3';
+export const DCA_API_VERSION = 'v3';
